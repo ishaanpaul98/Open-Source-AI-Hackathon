f2cb2c82f71618df93e037aaf575a9f7172393b4
Ishaan <ishaanpaul@hotmail.com>
2025-04-19 10:25:32 -0700

    Initial Commit
    Initial Commit

---
+++ agent.py
@@ -0,0 +1,25 @@
+import langchain 
+import os
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain.agents import Tool
+from langchain import hub
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import OpenAI
+import langgraph
+from dotenv import load_dotenv
+
+load_dotenv("secrets.env")
+
+# Access the variables
+api_key = os.getenv("API_KEY")
+
+from phoenix.otel import register
+
+"""
+# configure the Phoenix tracer
+tracer_provider = register(
+  project_name="hackathon-app", # Default is 'default'
+  auto_instrument=True # See 'Trace all calls made to a library' below
+)
+tracer = tracer_provider.get_tracer(__name__)
+"""
